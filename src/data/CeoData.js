6ec5742966f7eed87ad0d52f9ef52356d162ff96
Omar Ouhra <omarouhra@Omars-Mac-mini.local>
2021-04-26 00:07:00 +0000

    i

--- src/data/CeoData.js
+++ src/data/CeoData.js
@@ -1,15 +1,15 @@
-import topoImage from '../images/AboutUs-1.png';
-import CeoImg from '../images/CEO.JPG';
+import topoImage from "../images/AboutUs-1.png";
+import CeoImg from "../images/CEO.JPG";
 
 const CeoData = {
-    image :topoImage,
-    CeoImg : CeoImg,
-    CeoName:'Mr Mohammad Rafik',
-    Status: 'Expert foncier, ingernieure topographe et PDG ECOTOPO ',
-    CeoWord : '“Depuis 2006, EcoTopo projette son expertise sur l\'ensemble du territoire marocain. Nos grands partenaires ont soutenu non seulement notre réputation mais aussi la confiance de nos clients, et leur détermination dans le choix de notre société. L\'aspect pluridisciplinaire est bien évidemment activé à EcoTopo: Urbanisme, Travaux Publiques, Expertise foncière s\'inscrivent sous nos spécialités professionnelles. “ ',
-    reverse: false, 
-    delay: 100,
-    
+  image: topoImage,
+  CeoImg: CeoImg,
+  CeoName: "Mr Mohammad Rafik",
+  Status: "Expert foncier, ingénieur topographe et PDG ECOTOPO ",
+  CeoWord:
+    "“Depuis 2006, EcoTopo projette son expertise sur l'ensemble du territoire marocain. Nos grands partenaires ont soutenu non seulement notre réputation mais aussi la confiance de nos clients, et leur détermination dans le choix de notre société. L'aspect pluridisciplinaire est bien évidemment activé à EcoTopo: Urbanisme, Travaux Publiques, Expertise foncière s'inscrivent sous nos spécialités professionnelles. “ ",
+  reverse: false,
+  delay: 100,
 };
 
 export default CeoData;
