5fcf5d012cb6d2a90093ae48db8630dca1b799cc
Omar Ouhra <omarouhra@Omars-Mac-mini.local>
2021-04-27 16:45:41 +0000

    i

--- src/data/ServiceData.js
+++ src/data/ServiceData.js
@@ -1,14 +1,21 @@
-import ServiceImg from '../images/service-img.png'
-
-
+import ServiceImg from "../images/service-img.png";
 
 const ServiceData = {
-    image: ServiceImg,
-    title: 'Nos solutions sont toujours fiables, surtout personnalisées et répondent à vos différentes exigences.',
-    paragraph: 'Notre vocation inclut un catalogue large et complet de services topographiques. Notre parcours riche nous apporte une valeur ajoutée en termes  de compétences d\'accompagnement de projets pareils.',
-    ServiceList: [
-         '- Expertise fonciere' ,'- Plan côté','- Mise à jour' ,'- Travaux urbaines' ,'- Morcellement' ,'- Copropriété','- Messurage (projet pharmacie)','- Débit tabac' ,
-    ],
+  image: ServiceImg,
+  title:
+    "Nos solutions sont toujours fiables, surtout personnalisées et répondent à vos différentes exigences.",
+  paragraph:
+    "Notre vocation inclut un catalogue large et complet de services topographiques. Notre parcours riche nous apporte une valeur ajoutée en termes  de compétences d'accompagnement de projets pareils.",
+  ServiceList: [
+    "- Expertise fonciere",
+    "- Plan côté",
+    "- Mise à jour",
+    "- T.P & V.R.D.",
+    "- Morcellement",
+    "- Copropriété",
+    "- Messurage (projet pharmacie)",
+    "- Débit tabac",
+  ],
 };
 
 export default ServiceData;
